3d964a8871f2d0ccd55420b414b7b26430d83c65
Kevin Cox <49072061+kevinkvc34@users.noreply.github.com>
2021-01-11 11:18:20 -0600

    Added ping command with file organization

--- index.js
+++ index.js
@@ -2,11 +2,33 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const client = new Discord.Client();
 const config = require("./config/config.json");
+const ping = require('./resources/features/ping_command');
 
 client.login(config.BOT_TOKEN);
 
 client.on("ready", () => {
 
     console.log("Ready");
+
+})
+
+client.on("message", msg => {
+
+
+    if (msg.charAt(0) == ">" && msg.charAt(1) == ">")
+        {
+
+            var args = msg.split(" ").slice(1);
+            var command = msg.slice(2).split(" ")[0];
+            if (command.toLowerCase() == "ping")
+            {
+
+
+                //do the ping feature function
+                ping.pingPlayer(msg, args, client);
+
+            }
+
+        }
 
 })
--- resources/features/ping_command.js
+++ resources/features/ping_command.js
@@ -1,1 +1,37 @@
-const main = require('../../index');
+
+//check command, then args
+function pingPlayer(msg, args, client)
+{
+
+    if (args.length != 1)
+    {
+
+        msg.channel.send("Only one player is allowed!");
+        return;
+
+    }
+    else
+    {
+
+        if (msg.mentions.members.size() == 0)
+        {
+
+            msg.channel.send("Invalid user! Make sure to tag them!");
+            return;
+
+        }
+        else
+        {
+
+            msg.author.send(`You just got pinged <@${args[0]}`);
+            return;
+
+        }
+
+    }
+
+}
+
+module.exports = {
+    pingPlayer
+};
